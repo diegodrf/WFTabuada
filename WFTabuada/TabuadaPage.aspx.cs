8cc5a4134b11559dc99a4da29c84b37c0c254639
Diego Faria <diego.rdfaria@gmail.com>
2022-12-02 02:49:38 -0500

    Show result building a table

--- WFTabuada/TabuadaPage.aspx.cs
+++ WFTabuada/TabuadaPage.aspx.cs
@@ -9,6 +9,14 @@ namespace WFTabuada
 {
     public partial class TabuadaPage : System.Web.UI.Page
     {
+        private Table tbResult;
+
+        public TabuadaPage()
+        {
+            tbResult = new Table();
+        }
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if(!IsPostBack)
@@ -21,13 +29,40 @@ namespace WFTabuada
 
         protected void BtSubmitBaseNumber_Click(object sender, EventArgs e)
         {
-            LbResult.Items.Clear();
-            for(var n = 0; n <= 10; n++)
+            //LbResult.Items.Clear();
+            //for(var n = 0; n <= 10; n++)
+            //{
+            //    var baseNumber = int.Parse(DdBaseNumber.SelectedValue);
+            //    var text = $"{baseNumber} x {n} = {baseNumber * n}";
+            //    LbResult.Items.Add(new ListItem(text));
+            //}
+
+            tbResult.Rows.Clear();            
+
+            for (var n = 0; n <= 10; n++)
             {
-                var baseNumber = int.Parse(DdBaseNumber.SelectedValue);
-                var text = $"{baseNumber} x {n} = {baseNumber * n}";
-                LbResult.Items.Add(new ListItem(text));
+                var row = new TableRow();
+                
+                var factorA = new TableCell() { Text = DdBaseNumber.SelectedValue };
+                row.Cells.Add(factorA);
+                
+                var multiplySignal = new TableCell() { Text = "X" };
+                row.Cells.Add(multiplySignal);
+
+                var factorB = new TableCell() { Text = n.ToString() };
+                row.Cells.Add(factorB);
+
+                var equalSignal = new TableCell() { Text = "=" };
+                row.Cells.Add(equalSignal);
+
+                var result = int.Parse(DdBaseNumber.SelectedValue) * n;
+                var product = new TableCell() { Text = result.ToString() };
+                row.Cells.Add(product);
+
+                tbResult.Rows.Add(row);
             }
+
+            PhPlaceHolder.Controls.Add(tbResult);
         }
     }
 }
